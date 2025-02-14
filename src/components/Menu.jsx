a0e0512f98d08781295247a057cf218af1842bbc
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-14 15:09:54 +0200

    setup completion

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+`;
+
+function Menu() {
+  return <Container>Menu</Container>;
+}
+
+export default Menu;
