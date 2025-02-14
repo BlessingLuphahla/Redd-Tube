b171d11c22b05b6dc0354d71f41cd71d70f166c1
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-14 17:14:38 +0200

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,9 @@ const Container = styled.div`
   flex: 1;
   background-color: #202020;
   height: 100vh;
+  padding: 0;
+  margin: 0;
 `;
-
 
 function Menu() {
   return <Container>Menu</Container>;
