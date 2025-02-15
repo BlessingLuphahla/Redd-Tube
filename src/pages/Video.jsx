1ef260374afde6fb8fc3835106d3a144d0280ca8
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-16 00:33:17 +0200

    Video update

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,5 +1,104 @@
+import styled from "styled-components";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const VideoWrapper = styled.div``;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  outline: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 function Video() {
-  return <div>Video</div>;
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="620"
+            src="https://www.youtube.com/embed/M2WWYcVKvSc"
+            allowFullScreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>234, 121, 121 views • Jan 22, 2025</Info>
+          <ButtonContainer>
+            <Button>
+              <ThumbUpAltOutlinedIcon /> 123
+            </Button>
+            <Button>
+              <ThumbDownAltOutlinedIcon /> Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </ButtonContainer>
+        </Details>
+        <Hr />
+      </Content>
+      <Recommendation>Recomendation</Recommendation>
+    </Container>
+  );
 }
 
 export default Video;
