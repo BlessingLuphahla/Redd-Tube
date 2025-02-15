492e3f6f51fd81f6bd075820d3a7b15ee968176e
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-15 16:45:29 +0200

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,49 @@
+import styled from "styled-components";
+import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Container = styled.div``;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  width: 105px;
+  height: 45px;
+  background-color: transparent;
+  border: 1px solid #3eafff;
+  color: #3eafff;
+  border-radius: 3px;
+  outline: none;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+const Search = styled.div``;
+const Input = styled.input``;
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input type="search" placeholder="Search" />
+          <SearchIcon />
+        </Search>
+        <Button>
+          <PersonIcon />
+          Sign In
+        </Button>
+      </Wrapper>
+    </Container>
+  );
 }
 
-export default Navbar
+export default Navbar;
